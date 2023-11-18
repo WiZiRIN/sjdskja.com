63ea8f909b1a096dcc86355dbe91d49ea07cea50
WiZiRIN <151285008+WiZiRIN@users.noreply.github.com>
2023-11-17 20:33:44 -0500

    afaf
    ffa

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function cambiaM() {
+    document.getElementById("loading-message").innerText = "TE QUIERO MUCHO!!!"
+}
+
+setTimeout(cambiaM,2300)
+
+setTimeout(function() {
+    alert("RECIBISTE UN ABRAZO!!!!")
+}, 2400)
